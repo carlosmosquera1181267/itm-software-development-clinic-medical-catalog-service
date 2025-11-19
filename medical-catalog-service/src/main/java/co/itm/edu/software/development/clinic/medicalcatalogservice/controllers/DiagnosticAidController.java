595f55ea268cc48ec8794dc941f869258e43538b
carlosmosquera1181267carlosm <carlosmosquera1181267@correo.itm.edu.co>
2025-11-19 02:26:47 -0500

    fix imports

--- medical-catalog-service/src/main/java/co/itm/edu/software/development/clinic/medicalcatalogservice/controllers/DiagnosticAidController.java
+++ medical-catalog-service/src/main/java/co/itm/edu/software/development/clinic/medicalcatalogservice/controllers/DiagnosticAidController.java
@@ -3,7 +3,6 @@ package co.itm.edu.software.development.clinic.medicalcatalogservice.controllers
 import co.itm.edu.software.development.clinic.medicalcatalogservice.dto.DiagnosticAid;
 import co.itm.edu.software.development.clinic.medicalcatalogservice.service.DiagnosticAidService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- medical-catalog-service/src/main/java/co/itm/edu/software/development/clinic/medicalcatalogservice/controllers/SpecialityController.java
+++ medical-catalog-service/src/main/java/co/itm/edu/software/development/clinic/medicalcatalogservice/controllers/SpecialityController.java
@@ -3,7 +3,6 @@ package co.itm.edu.software.development.clinic.medicalcatalogservice.controllers
 import co.itm.edu.software.development.clinic.medicalcatalogservice.dto.Specialty;
 import co.itm.edu.software.development.clinic.medicalcatalogservice.service.SpecialtyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
